7020dde635b928a130c2511f05b803dfec72fd0a
Matheus_DeLima <matheuslimaaa13@gmail.com>
2019-03-24 15:34:21 -0300

    tela detalhes do evento v1

--- lib/src/agenda/evento_card.dart
+++ lib/src/agenda/evento_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:siepex/models/evento.dart';
+import 'package:siepex/src/agenda/evento_detalhes.dart';
 
 class EventoCard extends StatefulWidget {
   final Widget child;
@@ -12,48 +13,53 @@ class EventoCard extends StatefulWidget {
 class _EventoCardState extends State<EventoCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.grey[200],
-      elevation: 10,
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-                flex: 1,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                        // margin: EdgeInsets.only(left: 20, right: 10, top: 15),
-                        width: 70,
-                        height: 70,
-                        decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: new DecorationImage(
-                                fit: BoxFit.cover,
-                                image: new NetworkImage(
-                                    "https://avatars0.githubusercontent.com/u/29609021?s=400&u=be91d738c1796c1f523b5c630c1359956d170ccb&v=4")))),
-                    Text(
-                      'Autor da palestra',
-                      textAlign: TextAlign.center,
-                    )
-                  ],
-                )),
-            Expanded(
-              flex: 3,
-              child: Container(
-                  margin: EdgeInsets.only(top: 0),
-                  child: ListTile(
-                    title: Text('Uma palestra bacana e complexa'),
-                    subtitle: Text('00:00-00:30\nPalco tal'),
-                  )),
-            )
-          ],
-        ),
-      ),
-      shape: RoundedRectangleBorder(
-          side: BorderSide(color: Colors.green, style: BorderStyle.solid),
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-    );
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => EventoDetalhes()));
+        },
+        child: Card(
+          color: Colors.grey[200],
+          elevation: 10,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            // margin: EdgeInsets.only(left: 20, right: 10, top: 15),
+                            width: 70,
+                            height: 70,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: new NetworkImage(
+                                        "https://avatars0.githubusercontent.com/u/29609021?s=400&u=be91d738c1796c1f523b5c630c1359956d170ccb&v=4")))),
+                        Text(
+                          'Autor da palestra',
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    )),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                      margin: EdgeInsets.only(top: 0),
+                      child: ListTile(
+                        title: Text('Uma palestra bacana e complexa'),
+                        subtitle: Text('00:00-00:30\nPalco tal'),
+                      )),
+                )
+              ],
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.green, style: BorderStyle.solid),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+        ));
   }
 }
---
+++ lib/src/agenda/evento_detalhes.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:siepex/models/evento.dart';
+import 'package:siepex/models/palestrante.dart';
+import 'package:siepex/src/agenda/evento_card.dart';
+
+class EventoDetalhes extends StatefulWidget {
+  final Widget child;
+
+  EventoDetalhes({Key key, this.child}) : super(key: key);
+
+  _EventoDetalhesState createState() => _EventoDetalhesState();
+}
+
+class _EventoDetalhesState extends State<EventoDetalhes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                "Palestra bacana",
+                style: TextStyle(),
+              ),
+              background: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.only(left: 5, right: 5),
+                      width: 100,
+                      height: 100,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: new DecorationImage(
+                              fit: BoxFit.cover,
+                              image: new NetworkImage(
+                                  "https://avatars0.githubusercontent.com/u/29609021?s=400&u=be91d738c1796c1f523b5c630c1359956d170ccb&v=4")))),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, -1.0),
+                        end: Alignment(0.0, -0.4),
+                        colors: <Color>[Color(0x60000000), Color(0x00000000)],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              AnnotatedRegion<SystemUiOverlayStyle>(
+                  value: SystemUiOverlayStyle.dark,
+                  child: ListTile(
+                      title: Text(
+                    "Descrição:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ))),
+              ListTile(
+                  title: Text.rich(TextSpan(
+                      text:
+                          """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. """))),
+              AnnotatedRegion<SystemUiOverlayStyle>(
+                  value: SystemUiOverlayStyle.dark,
+                  child: ListTile(
+                      title: Text(
+                    "Horario:",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ))),
+              ListTile(
+                title: Text('Dia 1'),
+                subtitle: Text('00:00-00:30'),
+              ),
+              AnnotatedRegion<SystemUiOverlayStyle>(
+                  value: SystemUiOverlayStyle.dark,
+                  child: ListTile(
+                      title: Text(
+                    "Palestrante(s):",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ))),
+              palestranteCard(new Palestrante(), context)
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget palestranteCard(Palestrante palestrante, BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => EventoDetalhes()));
+        },
+        child: Card(
+          color: Colors.grey[200],
+          elevation: 10,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            // margin: EdgeInsets.only(left: 20, right: 10, top: 15),
+                            width: 70,
+                            height: 70,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: new NetworkImage(
+                                        "https://avatars0.githubusercontent.com/u/29609021?s=400&u=be91d738c1796c1f523b5c630c1359956d170ccb&v=4")))),
+                        Text(
+                          'Autor da palestra',
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    )),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                      margin: EdgeInsets.only(top: 0),
+                      child: ListTile(
+                        title: Text('Uma palestra bacana e complexa'),
+                        subtitle: Text('00:00-00:30\nPalco tal'),
+                      )),
+                )
+              ],
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.green, style: BorderStyle.solid),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+        ));
+  }
+}
