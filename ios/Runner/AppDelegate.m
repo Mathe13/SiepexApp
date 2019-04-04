c42715bb4d9d46e4f72fecce7dfa3b349d1d57fa
Matheus_DeLima <matheuslimaaa13@gmail.com>
2019-04-03 23:06:10 -0300

    ajuste

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,8 +7,6 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
-  // [GMSServices provideAPIKey:@"AIzaSyAfCfCRZ_Pj1H9-dZkf8yt8objm9oWXZU8"];
-  // [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
